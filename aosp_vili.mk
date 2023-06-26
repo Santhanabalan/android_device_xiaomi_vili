b81aedc2d03787485ca061bb6ed899b6350be5f2
Santhanabalan <bala2102002@gmail.com>
2023-06-26 21:58:55 +0530

    vili: Pixelify!

--- aosp_vili.mk
+++ aosp_vili.mk
@@ -11,8 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from vili device
 $(call inherit-product, device/xiaomi/vili/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_AOSP_RECOVERY := true
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1440
@@ -21,7 +23,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := vili
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2107113SG
-PRODUCT_NAME := lineage_vili
+PRODUCT_NAME := aosp_vili
 
 # MIUI stuff
 TARGET_SHIPS_GALLERY=true
