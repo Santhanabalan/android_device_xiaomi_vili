81ef3c2510708f710ba4161a1ae3e4629d29f776
Santhanabalan <bala2102002@gmail.com>
2023-09-06 13:10:12 +0530

    vili: Initial Vili Specific Tree
    Rebrand from sm8350-common trees

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_vili.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_vili-user \
+    aosp_vili-userdebug \
+    aosp_vili-eng
---
+++ aosp_vili.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from vili device
+$(call inherit-product, device/xiaomi/vili/device.mk)
+
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_AOSP_RECOVERY := true
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := vili
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := 2107113SG
+PRODUCT_NAME := aosp_vili
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vili-user 13 RKQ1.211001.001 V14.0.10.0.TKDEUXM release-keys"
+
+BUILD_FINGERPRINT := "Xiaomi/vili/vili:13/RKQ1.211001.001/V14.0.10.0.TKDEUXM:user/release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
